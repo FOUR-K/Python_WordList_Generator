b5f81b8b537edc0d00c9155ab28baa676ca0bf95
fourK <innocenthacker84@gmail.com>
2018-10-10 19:12:24 +0530

    added argparse

--- wl_gen.py
+++ wl_gen.py
@@ -1,6 +1,10 @@
-import numpy as np 
+import numpy as np
+import argparse 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+    print("dummy program")
     pass
 
 if __name__=="__main__":

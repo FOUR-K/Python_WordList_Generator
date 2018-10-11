8d9ef095b355db55ea41058d4ccc541dcd5673e0
fourK <innocenthacker84@gmail.com>
2018-10-11 17:59:53 +0530

    Added " -c " argument to add command line input

--- wl_gen.py
+++ wl_gen.py
@@ -3,8 +3,17 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
-    print("dummy program")
+    parser.add_argument("-c",action='store_true',help="take word input in command line ")
+    args = parser.parse_args()
+
+    words=[]
+    if args.c:
+        print('Enter space separated words')
+        words = input().split()
+
+        print(words)
+    else:
+        print("dummy program")
     pass
 
 if __name__=="__main__":
